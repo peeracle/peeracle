00d1a5a15b41a10447c10b1a263adbd539dec1fa
Axel Isouard <ax@eml.cc>
2015-08-24 17:38:06 +0200

    TrackerClient: Connect, disconnect, message handlers

--- src/trackerClient.js
+++ src/trackerClient.js
@@ -21,9 +21,17 @@
  */
 
 // @exclude
+var WebSocket = require('websocket').w3cwebsocket;
 var Peeracle = {
   Listenable: require('./listenable'),
+  TrackerMessage: require('./trackerMessage')
 };
+/* eslint-disable */
+var window = {
+  setInterval: setInterval,
+  clearInterval: clearInterval
+};
+/* eslint-enable */
 // @endexclude
 
 /* eslint-disable */
@@ -31,17 +39,88 @@ Peeracle.TrackerClient = (function() {
   /* eslint-enable */
   /**
    * @class TrackerClient
-   * @memberof {Peeracle}
+   * @memberof Peeracle
    * @mixes Peeracle.Listenable
    * @constructor
+   * @param {String} url
    */
-  function TrackerClient() {
+  function TrackerClient(url) {
     Peeracle.Listenable.call(this);
+
+    this.ws = null;
+    this.url = url;
   }
 
   TrackerClient.prototype = Object.create(Peeracle.Listenable.prototype);
   TrackerClient.prototype.constructor = TrackerClient;
 
+  TrackerClient.prototype.onOpen = function onOpen() {
+    var msg = new Peeracle.TrackerMessage({
+      type: Peeracle.TrackerMessage.MessageType.Hello
+    });
+    var bytes = msg.serialize();
+    this.ws.send(bytes);
+  };
+
+  TrackerClient.prototype.onMessage = function onMessage(e) {
+    var msg = new Peeracle.TrackerMessage();
+    var bytes = new Uint8Array(e.data);
+
+    if (!bytes || !bytes.length) {
+      return;
+    }
+
+    msg.unserialize(bytes);
+    this['handle' + Peeracle.TrackerMessage.Types[msg.props.type].name]
+      .bind(this)(msg);
+  };
+
+  TrackerClient.prototype.onError = function onError() {
+  };
+
+  TrackerClient.prototype.onClose = function onClose(e) {
+    this.emit('disconnect', e.code, e.reason);
+  };
+
+  TrackerClient.prototype.connect = function connect() {
+    this.ws = new WebSocket(this.url, 'prcl-0.0.1');
+    this.ws.binaryType = 'arraybuffer';
+    this.ws.onopen = this.onOpen.bind(this);
+    this.ws.onmessage = this.onMessage.bind(this);
+    this.ws.onerror = this.onError.bind(this);
+    this.ws.onclose = this.onClose.bind(this);
+  };
+
+  TrackerClient.prototype.announce = function announce(hash, got) {
+    var msg = new Peeracle.TrackerMessage({
+      type: Peeracle.TrackerMessage.MessageType.Announce,
+      hash: hash,
+      got: got
+    });
+    var bytes = msg.serialize();
+    this.ws.send(bytes);
+  };
+
+  TrackerClient.prototype.denounce = function denounce(hash) {
+    var msg = new Peeracle.TrackerMessage({
+      type: Peeracle.TrackerMessage.MessageType.Denounce,
+      hash: hash
+    });
+    var bytes = msg.serialize();
+    this.ws.send(bytes);
+  };
+
+  TrackerClient.prototype.handleWelcome = function handleWelcome(msg) {
+    this.emit('connect', msg.props.id);
+  };
+
+  TrackerClient.prototype.handleEnter = function handleEnter(msg) {
+    this.emit('enter', msg.props.hash, msg.props.id, msg.props.got);
+  };
+
+  TrackerClient.prototype.handleLeave = function handleLeave(msg) {
+    this.emit('leave', msg.props.hash, msg.props.id);
+  };
 
   return TrackerClient;
 })();
